bfd5711b64b0b1c257d2b2836d304945bd06d976
Yutori_Natsu <153736506+YutoriNatsu@users.noreply.github.com>
2025-06-03 13:47:48 +0800

    Add Lab-03

---
+++ Lab03-ViT-LLaVA/inference.py
@@ -0,0 +1,33 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+
+# llava-1.5预训练权重
+model_path = "ckpt/llava"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path)
+)
+
+# 文本提示
+prompt = "Descripe what is in this picture"
+# 测试图片路径
+image_file = "images/input.jpg"
+
+args = type('Args', (), {
+    "model_path": model_path,
+    "model_base": None,
+    "model_name": get_model_name_from_path(model_path),
+    "query": prompt,
+    "conv_mode": None,
+    "image_file": image_file,
+    "sep": ",",
+    "temperature": 0,
+    "top_p": None,
+    "num_beams": 1,
+    "max_new_tokens": 512
+})()
+
+eval_model(args)
